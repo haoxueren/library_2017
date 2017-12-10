214d32a6c2480fce0801c4569727bb95e8494818
HaoMingliang <haoxueren@foxmail.com>
2017-12-10 22:06:10 +0800

    [Add]添加Empty类，定义了一些常用的空对象。

---
+++ library/src/main/java/com/haoxueren/library/Empty.java
@@ -0,0 +1,15 @@
+package com.haoxueren.library;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Define Some Empty Value
+ * Created by Haoxueren on 2017/12/10.
+ */
+public interface Empty {
+
+    String STRING = ""; // 空字符串
+
+    List LIST = Collections.EMPTY_LIST; // 空集合
+}
