46191211d1d3b2c134eaaab46446418618b7047b
HaoMingliang <haoxueren@foxmail.com>
2017-12-24 00:50:46 +0800

    [Feature]添加了DateHelper类。

---
+++ library/src/main/java/com/haoxueren/library/DateHelper.java
@@ -0,0 +1,25 @@
+package com.haoxueren.library;
+
+import android.text.TextUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * 时间/日期 帮助类
+ * Created by Haoxueren on 2017/12/24.
+ */
+public class DateHelper {
+
+    public static String format(Date date, String pattern) {
+        if (date == null) {
+            return "date is null";
+        }
+        if (TextUtils.isEmpty(pattern)) {
+            return "empty pattern";
+        }
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern, Locale.CHINA);
+        return dateFormat.format(date);
+    }
+}
